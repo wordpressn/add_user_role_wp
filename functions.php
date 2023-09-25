01c802becb5dc0762b68e460283aee7d61cf42c6
Fidatok Rahman <138792260+wordpressn@users.noreply.github.com>
2023-09-25 19:54:58 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+add_role('moderator', 'Moderator', array(
+'read' => true,
+'create_posts' => true,
+'edit_posts' => true,
+'edit_others_posts' => true,
+'publish_posts' => true,
+'manage_categories' => true,
+));
